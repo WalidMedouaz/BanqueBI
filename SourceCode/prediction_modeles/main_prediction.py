ed6eb2125916a5861653cae84057652e6c967d0f
Rechidi Abdelghani <ghanipro84@gmail.com>
2024-11-02 11:32:04 +0100

    Ajout du modèle SVM (ainsi que quelques modifications liés à un warning .ravel())
    - SVM_functions.py
    - main_prediction.py
    - Ajout de .ravel() 2D->1D
    - Ajout du dossier SVM pour les plots liés à ce modèle

--- SourceCode/prediction_modeles/main_prediction.py
+++ SourceCode/prediction_modeles/main_prediction.py
@@ -7,15 +7,17 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 import matplotlib.pyplot as plt
 
 import kNN_functions as kf
+import SVM_functions as sF
 
 
 # Charger les données prétraitées
 X_train = pd.read_csv("../data_after_pretraitement/X_train.csv")
 X_test = pd.read_csv("../data_after_pretraitement/X_test.csv")
-y_train = pd.read_csv("../data_after_pretraitement/y_train.csv")
-y_test = pd.read_csv("../data_after_pretraitement/y_test.csv")
+y_train = pd.read_csv("../data_after_pretraitement/y_train.csv").values.ravel()
+y_test = pd.read_csv("../data_after_pretraitement/y_test.csv").values.ravel()
 x_val = pd.read_csv("../data_after_pretraitement/X_val.csv")
-y_val = pd.read_csv("../data_after_pretraitement/y_val.csv")
+y_val = pd.read_csv("../data_after_pretraitement/y_val.csv").values.ravel()
+
 
 
 print("###################################################################") # ----------------------------------------------------------------- # 
@@ -46,6 +48,38 @@ print("###################################################################") # -
 # Bayes
 print("###################################################################") # ----------------------------------------------------------------- # 
 # SVM
+# Modèle : Plutot lent et pas optimisé pour les grands jeux de données
+# Définition d'une liste de noyaux pour tester différents modèles SVM
+kernels = ['linear', 'rbf', 'poly', 'sigmoid']  # On peut ajouter d'autres noyaux si besoin :
+#                             ['linear', 'poly', 'rbf', 'sigmoid']
+
+# Mettre en commentaire ça si tu veux test sur le jeu de données initial (prend plus de temps, modifie sur les inputs en bas (enlever le small))
+X_train_small = X_train.sample(frac=0.1, random_state=42)  # 10% des données
+y_train_small = y_train[:len(X_train_small)]
+
+svm_accuracies = []
+
+# Boucle pour tester chaque noyau
+for kernel in kernels:
+    # Appel de la fonction svm_prediction et obtenir la précision pour chaque noyau
+    acc = sF.svm_prediction(X_train_small, X_test, y_train_small, y_test, kernel)
+    # Ajout de la précision obtenue à la liste des précisions
+    svm_accuracies.append(acc)
+
+# Affichage de la précision moyenne pour les différents noyaux SVM testés
+print(f"Précision moyenne pour SVM: {np.mean(svm_accuracies):.2f}")
+
+# Plot de la précision pour chaque noyau
+plt.figure(figsize=(10, 6))
+plt.plot(kernels, svm_accuracies, marker='o', linestyle='-')
+plt.xlabel('Type de noyau SVM')
+plt.ylabel('Précision')
+plt.title('Précision du modèle SVM pour différents noyaux')
+plt.grid(True)
+plt.savefig("../../Analyse/plot_predictions/SVM/svm_accuracy_per_kernel.png")
+plt.show()
+
+
 print("###################################################################") # ----------------------------------------------------------------- # 
 # Tree
 print("###################################################################") # ----------------------------------------------------------------- # 
